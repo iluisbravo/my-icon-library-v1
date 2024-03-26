3e657a6f5c9ba1ca5486623a8ebc9e7ec5bc32ac
Luis Bravo <luis.bravo@100ladrillos.com>
2024-03-26 10:43:47 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import { Icon1 } from "./src/icons/Icon1";
+import { Icon2 } from "./src/icons/Icon2";
+
+export { Icon1, Icon2 };
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import { ReactComponent as Icon1 } from "./icons/Icon1";
-import { ReactComponent as Icon2 } from "./icons/Icon2";
-
-export { Icon1, Icon2 };
