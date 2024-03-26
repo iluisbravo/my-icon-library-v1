2b25901f865c6e08cc797c0eba42acedfc8d466d
Luis Bravo <luis.bravo@100ladrillos.com>
2024-03-25 18:25:18 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default as Icon1 } from './components/Icon1';
-export { default as Icon2 } from './components/Icon2';
-export { default as Icon3 } from './components/Icon3';
+export { default as Icon1 } from './icons/Icon1.svg';
+export { default as Icon2 } from './icons/Icon2.svg';
+export { default as Icon3 } from './icons/Icon3.svg';
